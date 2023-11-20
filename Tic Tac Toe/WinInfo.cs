946e896f6ed439057e7ddea73b85da46db9fff06
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-20 08:32:29 +0000

    Creating Data classes

---
+++ Tic Tac Toe/WinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tic_Tac_Toe.Enums;
+
+namespace Tic_Tac_Toe
+{
+    public class WinInfo
+    {
+        public WinType Type { get; set; }
+        public int Number { get; set; }
+    }
+}
