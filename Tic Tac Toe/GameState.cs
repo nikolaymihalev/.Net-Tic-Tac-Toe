2ecba387abfcf10b86767ac7386c29d976f22cb9
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-20 08:39:09 +0000

    Create actions properties and can make move method

--- Tic Tac Toe/GameState.cs
+++ Tic Tac Toe/GameState.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tic_Tac_Toe.Data;
 using Tic_Tac_Toe.Enums;
 
 namespace Tic_Tac_Toe
@@ -13,5 +14,22 @@ namespace Tic_Tac_Toe
         public Player CurrentPlayer { get; set; }
         public int TurnPassed { get; set; }
         public bool GameOver { get; set; }
+
+        public event Action<int, int> MoveMade;
+        public event Action<GameResult> GameEnded;
+        public event Action GameRestarted;
+
+        public GameState()
+        {
+            GameGrid = new Player[3, 3];
+            CurrentPlayer = Player.X;
+            TurnPassed = 0;
+            GameOver = false;
+        }
+
+        bool CanMakeMove(int r,int c) 
+        {
+            return !GameOver && GameGrid[r, c] == Player.None;
+        }
     }
 }
